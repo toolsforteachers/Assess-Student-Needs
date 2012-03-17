5266ac5d48dd0f947225f119e7d3aecf2c691fac
Toby Privett <toby@snaplab.co.uk>
2012-03-17 12:28:13 +0000

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Acn::Application.config.secret_token = '7e4148095a4f4d7808bcadfba74ce6b509a1a313bc15fd7f66a364fd742f0b872dbebf1c2cc68b504a0a558ce872711ec25c98a097fcf1788d294dee6946eb5a'
