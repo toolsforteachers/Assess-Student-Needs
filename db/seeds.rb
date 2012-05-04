bf6486359327503f0ac32638d8257577b3da4cc9
Toby Privett <toby@snaplab.co.uk>
2012-05-04 08:34:37 -0500

    tweak lesson demo

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,7 +9,7 @@
 teacher = Teacher.create( name: 'Toby P.', email: 'toby@snaplab.co.uk', password: 'password', password_confirmation: 'password' )
 nick = Teacher.create( name: 'Nick T.', email: 'nick@smallfish.net', password: 'f1shes', password_confirmation: 'f1shes' )
 
-Indicator.create(level: 4, name: 'Understand place value in numbers to 1000')
+Indicator.create(level: 4, name: 'Understand how to place value in numbers to 1000')
 Indicator.create(level: 3, name: 'Recognise negative numbers in contexts such as temperature')
 
 alice = Student.create(name: 'Alice')
