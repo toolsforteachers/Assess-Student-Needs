ff43973050fcedb266f7c2d3a4458455ab4a05ed
Toby Privett <toby@snaplab.co.uk>
2012-10-24 15:57:40 +0100

    Group names is unique

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,6 +2,6 @@ class Group < ActiveRecord::Base
   has_many :group_students
   has_many :students, through: :group_students
   has_many :lessons
-
+  validates_uniqueness_of :name
   default_scope order('name')
 end
