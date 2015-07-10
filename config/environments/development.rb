345b74e81e081e7bba929dbcb90a0a5638ba8129
Toby Privett <toby@snaplab.co.uk>
2015-07-10 13:47:43 +0100

    Rails 4.2: development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -6,8 +6,9 @@ Acn::Application.configure do
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
 
-  # Log error messages when you accidentally call methods on nil.
-  config.whiny_nils = true
+  # RAILS 4.2 upgrader
+  config.eager_load = false
+  config.active_record.migration_error = :page_load
 
   # Show full error reports and disable caching
   config.consider_all_requests_local       = true
@@ -21,15 +22,9 @@ Acn::Application.configure do
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
 
-  # Only use best-standards-support built into browsers
-  config.action_dispatch.best_standards_support = :builtin
-
-  # Raise exception on mass assignment protection for Active Record models
-  config.active_record.mass_assignment_sanitizer = :strict
-
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
-  config.active_record.auto_explain_threshold_in_seconds = 0.5
+  # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
   # Do not compress assets
   config.assets.compress = false
