8e60c2942abcdb5bd6a0f2cf9896dcd5c8b65945
Toby Privett <toby@snaplab.co.uk>
2015-07-10 14:08:18 +0100

    Rails 4: config updates

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -8,8 +8,11 @@ Acn::Application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
+  # Rails 4:
+  config.eager_load = true
+
   # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = false
+  config.serve_static_files = false
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
