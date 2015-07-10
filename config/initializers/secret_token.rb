b97919f4c04d283dfee56ab0321a2b9b1b63293f
Toby Privett <toby@snaplab.co.uk>
2015-07-10 14:32:18 +0100

    Initialiser update: secret_token -> secret_key_base

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Acn::Application.config.secret_token = '7e4148095a4f4d7808bcadfba74ce6b509a1a313bc15fd7f66a364fd742f0b872dbebf1c2cc68b504a0a558ce872711ec25c98a097fcf1788d294dee6946eb5a'
+Acn::Application.config.secret_key_base = '7e4148095a4f4d7808bcadfba74ce6b509a1a313bc15fd7f66a364fd742f0b872dbebf1c2cc68b504a0a558ce872711ec25c98a097fcf1788d294dee6946eb5a'
