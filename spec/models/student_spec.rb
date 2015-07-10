e42a4901609abd551094fdf6d8c76d7bf0e3bfe0
Toby Privett <toby@snaplab.co.uk>
2015-07-10 14:41:38 +0100

    RSpec should -> expect

--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -15,7 +15,7 @@ describe Student do
     end
 
     context 'without assessments' do
-      it { @student.current_level.should eql(1) }
+      it { expect(@student.current_level).to eql(1) }
     end
 
     context 'with a prior_knowledge_level of 3 and a level one assessment' do
@@ -24,7 +24,7 @@ describe Student do
         @student.update_attribute(:prior_knowledge_level, 3)
       end
 
-      it { @student.current_level.should eql(4) }
+      it { expect(@student.current_level).to eql(4) }
     end
 
     context 'with a prior_knowledge_level of 1 and a level three assessment' do
@@ -33,17 +33,17 @@ describe Student do
         @student.update_attribute(:prior_knowledge_level, 1)
       end
 
-      it { @student.current_level.should eql(3) }
+      it { expect(@student.current_level).to eql(3) }
     end
 
     context 'with a level one assessment' do
       before { Fabricate(:level_1_assessment, group: @group) }
-      it { @student.current_level.should eql(1) }
+      it { expect(@student.current_level).to eql(1) }
     end
 
     context 'with a level three assessment' do
       before { Fabricate(:level_3_assessment, group: @group) }
-      it { @student.current_level.should eql(3) }
+      it { expect(@student.current_level).to eql(3) }
     end
 
     context 'with a level one and a level three assessment' do
@@ -51,7 +51,7 @@ describe Student do
         Fabricate(:level_1_assessment, group: @group)
         Fabricate(:level_3_assessment, group: @group)
       end
-      it { @student.current_level.should eql(3) }
+      it { expect(@student.current_level).to eql(3) }
     end
   end
 end
