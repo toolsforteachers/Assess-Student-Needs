50c16585cd74139f274f27e20a274f849e34bf30
Toby Privett <toby@snaplab.co.uk>
2015-07-10 14:06:26 +0100

    Rails 4: schema.rb gets updated

--- db/schema.rb
+++ db/schema.rb
@@ -9,80 +9,83 @@
 # from scratch. The latter is a flawed and unsustainable approach (the more migrations
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
-# It's strongly recommended to check this file into your version control system.
+# It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121110170833) do
+ActiveRecord::Schema.define(version: 20121110170833) do
 
-  create_table "assessment_students", :force => true do |t|
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "assessment_students", force: :cascade do |t|
     t.integer  "assessment_id"
     t.integer  "student_id"
-    t.datetime "created_at",                                  :null => false
-    t.datetime "updated_at",                                  :null => false
-    t.integer  "score",                        :default => 0
-    t.string   "notes",         :limit => 500
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "score",                     default: 0
+    t.string   "notes",         limit: 500
   end
 
-  create_table "assessments", :force => true do |t|
+  create_table "assessments", force: :cascade do |t|
     t.string   "name"
     t.integer  "teacher_id"
     t.integer  "group_id"
-    t.datetime "created_at",      :null => false
-    t.datetime "updated_at",      :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.datetime "lesson_datetime"
     t.integer  "indicator_id"
     t.string   "type"
     t.string   "notes"
   end
 
-  create_table "group_students", :force => true do |t|
+  create_table "group_students", force: :cascade do |t|
     t.integer  "group_id"
     t.integer  "student_id"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
-  create_table "groups", :force => true do |t|
+  create_table "groups", force: :cascade do |t|
     t.string   "name"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
-  create_table "indicators", :force => true do |t|
+  create_table "indicators", force: :cascade do |t|
     t.string   "name"
     t.integer  "level"
-    t.datetime "created_at",  :null => false
-    t.datetime "updated_at",  :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.string   "key"
     t.text     "description"
     t.integer  "position"
   end
 
-  add_index "indicators", ["key"], :name => "index_indicators_on_key"
+  add_index "indicators", ["key"], name: "index_indicators_on_key", using: :btree
 
-  create_table "students", :force => true do |t|
+  create_table "students", force: :cascade do |t|
     t.string   "name"
-    t.datetime "created_at",            :null => false
-    t.datetime "updated_at",            :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.integer  "prior_knowledge_level"
   end
 
-  create_table "teachers", :force => true do |t|
-    t.string   "email",                  :default => "", :null => false
-    t.string   "encrypted_password",     :default => "", :null => false
+  create_table "teachers", force: :cascade do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          :default => 0
+    t.integer  "sign_in_count",          default: 0
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at",                             :null => false
-    t.datetime "updated_at",                             :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.string   "name"
   end
 
-  add_index "teachers", ["email"], :name => "index_teachers_on_email", :unique => true
-  add_index "teachers", ["reset_password_token"], :name => "index_teachers_on_reset_password_token", :unique => true
+  add_index "teachers", ["email"], name: "index_teachers_on_email", unique: true, using: :btree
+  add_index "teachers", ["reset_password_token"], name: "index_teachers_on_reset_password_token", unique: true, using: :btree
 
 end
