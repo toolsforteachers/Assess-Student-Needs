27ea0647f30fdf2cfa335adbab39448e71be2061
Toby Privett <toby@snaplab.co.uk>
2015-07-13 16:09:17 +0100

    "Sign in" has become "Log in"

--- features/step_definitions/teacher_steps.rb
+++ features/step_definitions/teacher_steps.rb
@@ -3,7 +3,7 @@ Given /^I am the teacher$/ do
   visit '/teachers/sign_in'
   fill_in "teacher_email", :with => @teacher.email
   fill_in "teacher_password", :with => @teacher.password
-  click_button "Sign in"
+  click_button "Log in"
 end
 
 Given /^I am the teacher of "([^"]*)" with students "([^"]*)"$/ do |group_name, student_names|
