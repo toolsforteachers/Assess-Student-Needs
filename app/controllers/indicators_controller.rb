038843b190ba633acd4a6a0e24583950ec828b94
Toby Privett <toby@snaplab.co.uk>
2015-07-13 17:02:02 +0100

    Change 'scoped' to 'all'
    - this should load an active relation
    object as opposed to scary everything
    (that 'all' would imply)

--- app/controllers/indicators_controller.rb
+++ app/controllers/indicators_controller.rb
@@ -1,6 +1,6 @@
 class IndicatorsController < ProtectedController
   def index
-    @indicators = Indicator.scoped
+    @indicators = Indicator.all
     @indicators = @indicators.where(:level => params[:level]) if params[:level]
     @indicators = @indicators.where(:key => params[:key]) if params[:key]
   end
