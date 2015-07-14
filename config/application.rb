ecf02d78fbc788d7d3e975c882091e8294d4a832
Toby Privett <toby@snaplab.co.uk>
2015-07-14 11:53:18 +0100

    Updates to the asset pipeline

--- config/application.rb
+++ config/application.rb
@@ -52,9 +52,7 @@ module Acn
 
     # Enable the asset pipeline
     config.assets.enabled = true
-    config.assets.initialize_on_precompile = false
-
-    # Version of your assets, change this if you want to expire all your assets
-    config.assets.version = '1.0'
+    config.serve_static_assets = true
+    config.assets.version = '1.1'
   end
 end
