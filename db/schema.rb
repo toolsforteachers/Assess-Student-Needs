535e764011277a6bc8426659cfe703aa451b246b
Toby Privett <toby@snaplab.co.uk>
2015-07-22 14:22:58 +0100

    Students can be softly deleted

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150722092306) do
+ActiveRecord::Schema.define(version: 20150722132014) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -85,7 +85,10 @@ ActiveRecord::Schema.define(version: 20150722092306) do
     t.datetime "created_at",                        null: false
     t.datetime "updated_at",                        null: false
     t.integer  "prior_knowledge_level"
+    t.datetime "deleted_at"
   end
+
+  add_index "students", ["deleted_at"], name: "index_students_on_deleted_at", using: :btree
 
   create_table "teachers", force: :cascade do |t|
     t.string   "email",                  limit: 255, default: "", null: false
