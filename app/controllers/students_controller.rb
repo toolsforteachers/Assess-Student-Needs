f206b1d7dfc2c2ff678bec4ba4b3e1bbd4229e2f
Toby Privett <toby@snaplab.co.uk>
2015-07-22 15:47:29 +0100

    Prevent adding a student outside the group form

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -12,31 +12,12 @@ class StudentsController < ProtectedController
     @level = (params[:level] || @student.current_level).to_i
   end
 
-  def new
-    @student = Student.new
-    if params[:lesson_id]
-      @assessment = Lesson.find(params[:lesson_id])
-      @student.lesson_id = @assessment.id if @assessment
-      @student.use_lesson_id = true
-      @student.groups << @assessment.group
-    end
-  end
-
   def update
     @student = Student.find(params[:id])
     if @student.update_attributes(student_params)
       redirect_to students_path, notice: 'Student was successfully updated.'
     else
       render action: 'edit'
-    end
-  end
-
-  def create
-    @student = Student.new(student_params)
-    if @student.save
-      redirect_to students_path, notice: 'Student was successfully created.'
-    else
-      render action: "new"
     end
   end
 
