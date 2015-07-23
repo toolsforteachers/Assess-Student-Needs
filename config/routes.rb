91f7824416e3fec08525ab1d42f8a51d632ea037
Toby Privett <toby@snaplab.co.uk>
2015-07-23 17:36:53 +0100

    Remove assessment_students routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,11 @@ Acn::Application.routes.draw do
   devise_for :teachers
 
   resources :groups do
-   resources :assessments
-   resources :lessons
-   resources :students
-   resources :teacher_judgements, controller: :assessments
+    resources :assessments
+    resources :lessons
+    resources :students
+    resources :teacher_judgements, controller: :assessments
   end
-  resources :assessment_students, :indicators
 
   root to: 'home#show'
 end
