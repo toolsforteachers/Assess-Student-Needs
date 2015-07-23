38be4b3d3157a42f6040d6198f12d68d957a63c0
Toby Privett <toby@snaplab.co.uk>
2015-07-23 16:17:03 +0100

    Add inidicators via a dropdown (temporarily)

--- features/step_definitions/lesson_steps.rb
+++ features/step_definitions/lesson_steps.rb
@@ -1,22 +1,26 @@
 Given(/^I add a new lesson for "(.*?)"$/) do |group_name|
+  Fabricate(:indicator, name: 'bar')
+  Fabricate(:indicator, name: 'foo')
+
   group = Group.find_by(name: group_name)
   visit new_group_lesson_path(group)
+
   fill_in('Goal', with: 'Do stuff')
-  fill_in 'Objective', with: '1'
+  select 'foo', from: 'Objective'
   click_link 'Add an objective'
-  within(page.all(:css, '.panel.objective').last) do
-    fill_in 'Objective', with: '2'
+  within(page.all(:css, '.well.objective').last) do
+    select 'bar', from: 'Objective'
   end
   click_button "Save"
 end
 
 Then(/^the lesson should have (\d+) objectives$/) do |objective_count|
-  page.should have_css('tr.objective', count: objective_count)
+  page.should have_css('.well.objective', count: objective_count)
 end
 
 When(/^I edit the lesson$/) do
-  click_link('edit lesson')
-  within(page.all(:css, '.panel.objective').last) do
+  click_link('Edit lesson')
+  within(page.all(:css, '.well.objective').last) do
     click_link('Remove')
   end
   click_button "Save"
