e8e7d4a7451b30e11fd08f47b4c91de74867b6d9
Toby Privett <toby@snaplab.co.uk>
2015-07-24 13:29:43 +0100

    Test the display of previous assessment attempts

--- features/step_definitions/score_steps.rb
+++ features/step_definitions/score_steps.rb
@@ -16,3 +16,18 @@ Then /^I should see "([^"]*)" has (\d+) ticks for "([^"]*)"$/ do |student_name, 
     find_field("assessment_score_#{ ticks }").should be_checked
   end
 end
+
+When(/^"(.*?)" already has a score of (\d+) for "(.*?)"$/) do |student_name, score, indicator_name|
+  indicator = Indicator.find_by(name: indicator_name)
+  student = Student.find_by(name: student_name)
+  Fabricate(:assessment, score: score,
+    indicator: indicator, student: student)
+end
+
+Then(/^I should see (\d+) for the last attempt by "(.*?)"$/) do |score, student_name|
+  student = Student.find_by_name(student_name)
+
+  within(:css, "table#student_#{ student.id } td.attempt") do
+    page.should have_text(score)
+  end
+end
--- features/teacher_assesses_lesson.feature
+++ features/teacher_assesses_lesson.feature
@@ -10,12 +10,14 @@ Feature: Teacher assesses a lesson
       | 2     | Adding up       | MA2_NUMBERS   | about the indicator |
       | 4     | Measuring stuff | MA3A_MEASURES |                     |
     And I add a lesson "Adding Up" for "Year 6A" with indicators "Adding up,Measuring stuff"
+    And "Ann" already has a score of 4 for "Measuring stuff"
 
   @javascript
   Scenario: recording student scores
     When I record a score of 5 for "Ann" for "Measuring stuff"
     And I wait 1 second
     Then I should see "Ann" has 5 ticks for "Measuring stuff"
+    Then I should see 4 for the last attempt by "Ann"
 
     When I record a score of 3 for "Bob" for "Adding up"
     And I wait 1 second
