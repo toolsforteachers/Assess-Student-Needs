1c803ad3d0b5136e5ef6ec7eb388ddcc5e8f3264
Toby Privett <toby@snaplab.co.uk>
2015-07-24 15:07:08 +0100

    Order assessments by student name

--- app/models/assessment.rb
+++ app/models/assessment.rb
@@ -2,4 +2,6 @@ class Assessment < ActiveRecord::Base
   belongs_to :assessor, polymorphic: true
   belongs_to :indicator
   belongs_to :student
+
+  default_scope { includes(:student).order('students.name') }
 end
