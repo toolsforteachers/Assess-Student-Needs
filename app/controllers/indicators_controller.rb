77b5981b8f0d86ec2880ab69c630bccfa5cbe7b5
Toby Privett <toby@snaplab.co.uk>
2015-07-24 15:21:20 +0100

    Remove unused code

--- app/controllers/indicators_controller.rb
+++
@@ -1,7 +0,0 @@
-class IndicatorsController < ProtectedController
-  def index
-    @indicators = Indicator.all
-    @indicators = @indicators.where(:level => params[:level]) if params[:level]
-    @indicators = @indicators.where(:key => params[:key]) if params[:key]
-  end
-end
