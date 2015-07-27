9dad2025214401089276f9137f6703c2cbc7607b
Toby Privett <toby@snaplab.co.uk>
2015-07-27 14:25:31 +0100

    Indicators *must* be subclassed

--- app/models/indicator.rb
+++ app/models/indicator.rb
@@ -1,4 +1,5 @@
 class Indicator < ActiveRecord::Base
   has_closure_tree
   validates :name, presence: :true
+  validates :type, presence: :true
 end
--- spec/models/indicator_spec.rb
+++ spec/models/indicator_spec.rb
@@ -2,4 +2,5 @@ require 'spec_helper'
 
 describe Indicator do
   it { should validate_presence_of(:name) }
+  it { should validate_presence_of(:type) }
 end
