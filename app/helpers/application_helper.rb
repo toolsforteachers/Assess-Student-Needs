3227d3a06fcc4624c3a3d7d6f6224caa2e247c33
Toby Privett <toby@snaplab.co.uk>
2015-07-30 16:21:30 +0100

    Style the assessments with bootstrap built in colors

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,14 @@ module ApplicationHelper
   def link_to_group
     link_to @group.name, group_path(@group)
   end
+
+  def likert_style(score)
+    case score.to_i
+      when 5 then 'success'
+      when 4 then 'info'
+      when 3 then 'active'
+      when 2 then 'warning'
+      when 1 then 'danger'
+    end
+  end
 end
