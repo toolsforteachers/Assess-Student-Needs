0aa8681ac5391c26dbc5b664e906a1f325c6ddf8
Toby Privett <toby@snaplab.co.uk>
2015-08-07 12:34:05 +0100

    Prefix group name with 'Class: '

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def link_to_group
-    link_to @group.name, group_path(@group)
+    link_to "Class: #{ @group.name }", group_path(@group)
   end
 
   def likert_style(score)
