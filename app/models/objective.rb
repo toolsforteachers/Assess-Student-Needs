5189763430db4d5692ed33630df76b59c7c35f7f
Toby Privett <toby@snaplab.co.uk>
2015-08-07 11:34:24 +0100

    Objectives have indicator attributes

--- app/models/objective.rb
+++ app/models/objective.rb
@@ -4,4 +4,27 @@ class Objective < ActiveRecord::Base
 
   validates :indicator_id, presence: :true
   validates :stream, length: { maximum: 15 }, presence: true
+
+  def objective
+    indicator.name
+  end
+
+  def level
+    indicator_ancestry.where(type: 'Indicators::Level').first.try(:name)
+  end
+
+  def topic
+    s = indicator_ancestry.where(type: 'Indicators::Topic').first.try(:name)
+    strand ? "#{ s } - #{ strand }" : s
+  end
+
+  protected
+
+  def indicator_ancestry
+    @indicator_ancestry ||= indicator.ancestors
+  end
+
+  def strand
+    indicator_ancestry.where(type: 'Indicators::Strand').first.try(:name)
+  end
 end
--- spec/models/objective_spec.rb
+++ spec/models/objective_spec.rb
@@ -6,4 +6,26 @@ RSpec.describe Objective, type: :model do
   it { should validate_presence_of(:indicator_id) }
   it { should validate_presence_of(:stream) }
   it { should validate_length_of(:stream).is_at_most(15) }
+
+  context 'indicator attributes' do
+    let(:i_subject) { Fabricate(:indicators_subject, name: 'Maths') }
+    let(:level) { Fabricate(:indicators_level, name: 'Year 1', parent: i_subject) }
+    let(:topic) { Fabricate(:indicators_topic, name: 'Number', parent: level) }
+    let(:strand) { Fabricate(:indicators_strand, name: 'addition and subtraction', parent: topic) }
+    let(:i_objective) { Fabricate(:indicators_objective, name: 'solve small problems', parent: strand) }
+
+    let(:objective) { Fabricate(:objective, indicator: i_objective) }
+
+    it 'has an objective' do
+      expect(objective.objective).to eql('solve small problems')
+    end
+
+    it 'has a topic' do
+      expect(objective.topic).to eql('Number - addition and subtraction')
+    end
+
+    it 'has a level' do
+      expect(objective.level).to eql('Year 1')
+    end
+  end
 end
