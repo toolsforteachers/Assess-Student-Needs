a2b5da2c3193931db3dd9697b57d6411bc52edda
Toby Privett <toby@snaplab.co.uk>
2015-08-10 11:09:20 +0100

    Seed Abigail

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,3 @@
 toby = Teacher.create( name: 'Toby P.', email: 'toby@snaplab.co.uk', password: 'password', password_confirmation: 'password' )
 nick = Teacher.create( name: 'Nick T.', email: 'nick@smallfish.net', password: 'f1shes', password_confirmation: 'f1shes' )
+abigail = Teacher.create( name: 'Abigail', email: 'abigail.jones@ymail.com', password: 'password', password_confirmation: 'password' )
