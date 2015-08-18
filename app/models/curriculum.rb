1e22f4217e089189440a76e853b91748c89ff72a
Toby Privett <toby@snaplab.co.uk>
2015-08-18 16:29:50 +0100

    Ensure curriculum names are unique

--- app/models/curriculum.rb
+++ app/models/curriculum.rb
@@ -2,6 +2,7 @@ class Curriculum < ActiveRecord::Base
   extend FriendlyId
   friendly_id :name, use: [:slugged, :finders]
 
+  validates :name, uniqueness: true
   has_many :subjects, class_name: 'Indicators::Subject'
 
   def to_s
--- spec/models/curriculum_spec.rb
+++ spec/models/curriculum_spec.rb
@@ -2,4 +2,5 @@ require 'spec_helper'
 
 RSpec.describe Curriculum, type: :model do
   it { should have_many(:subjects) }
+  it { should validate_uniqueness_of(:name) }
 end
