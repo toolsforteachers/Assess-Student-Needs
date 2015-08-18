f75747b52b086b7ce7c17821f2eb2fe0c91d3f0f
Toby Privett <toby@snaplab.co.uk>
2015-08-18 15:33:59 +0100

    Add a curriculum model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150817143931) do
+ActiveRecord::Schema.define(version: 20150818141902) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,12 @@ ActiveRecord::Schema.define(version: 20150817143931) do
     t.text     "notes"
     t.integer  "mark"
     t.integer  "out_of"
+  end
+
+  create_table "curricula", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "friendly_id_slugs", force: :cascade do |t|
