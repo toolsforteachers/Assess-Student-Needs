626eea98371f412985176dce88f92edf03a2a2a4
Toby Privett <toby@snaplab.co.uk>
2015-08-21 14:57:34 +0100

    Eager load levels and topics with the subject

--- app/controllers/discussions_controller.rb
+++ app/controllers/discussions_controller.rb
@@ -1,6 +1,6 @@
 class DiscussionsController < ProtectedController
   def show
-    @indicator = Indicator.find_by(slug: params[:indicator_id])
-    @subject = @indicator.ancestry.where(type: 'Indicators::Subject').first
+    @indicator = Indicator.includes(:self_and_descendants).find_by(slug: params[:indicator_id])
+    @subject = Indicators::Subject.where(id: @indicator.subject).includes(levels: [:topics]).first
   end
 end
