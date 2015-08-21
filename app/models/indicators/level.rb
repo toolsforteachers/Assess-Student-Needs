f9903e540730c644ebc728d65efe9ee6e17c018a
Toby Privett <toby@snaplab.co.uk>
2015-08-21 14:48:00 +0100

    Levels have_many topics

--- app/models/indicators/level.rb
+++ app/models/indicators/level.rb
@@ -1,9 +1,6 @@
 class Indicators::Level < Indicator
   validates :parent_id, presence: true
-
-  def topics
-    @topics ||= descendants.where(type: 'Indicators::Topic')
-  end
+  has_many :topics, class_name: 'Indicators::Topic', foreign_key: :parent_id
 
   def allowable_child_types
     [:topic]
