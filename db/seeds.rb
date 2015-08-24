8d6556ec64183b4124f0955b2a345e81c8fa3550
Toby Privett <toby@snaplab.co.uk>
2015-08-24 14:26:31 +0100

    Seed with fake students and the gds curricula

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,13 @@
+include Faker
+
 toby = Teacher.create( name: 'Toby P.', admin: true, email: 'toby@snaplab.co.uk', password: 'password', password_confirmation: 'password' )
 nick = Teacher.create( name: 'Nick T.', admin: true, email: 'nick@smallfish.net', password: 'f1shes', password_confirmation: 'f1shes' )
 abigail = Teacher.create( name: 'Abigail', admin: true, email: 'abigail.jones@ymail.com', password: 'password', password_confirmation: 'password' )
+
+c = Group.create(teacher: toby, name: "Toby's class")
+
+22.times do
+  Student.create(name: Faker::Name.name, group: c)
+end
+
+system 'rake asn:scrape_gds'
