8e9605bf67446bf0420d44c67a6d850a1e880a16
Toby Privett <toby@snaplab.co.uk>
2015-09-01 10:32:20 +0100

    Adding a Pedagogy service

---
+++ app/services/pedagogy_service.rb
@@ -0,0 +1,22 @@
+class PedagogyService < IndicatorService
+  class << self
+    def editable_by(teacher)
+      eager_load(teacher.pedagogies)
+    end
+
+    def viewable_by(teacher)
+      editable_by(teacher)
+    end
+
+    def first_topic(indicator_id)
+      indicator = find_indicator(indicator_id)
+      if indicator.is_a?(Indicators::Topic)
+        indicator
+      elsif indicator.is_a?(Indicators::Level)
+        indicator.topics.try(:first)
+      else
+        nil
+      end
+    end
+  end
+end
