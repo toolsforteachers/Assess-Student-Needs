36bd05b2052941839cee98331a41f769d6310f9a
Toby Privett <toby@snaplab.co.uk>
2015-09-01 12:16:07 +0100

    Prevent seeds running in production

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+raise 'No!' unless Rails.env.development?
 include Faker
 
 toby = Teacher.create( name: 'Tools for Teachers', email: 'toby@snaplab.co.uk', password: 'password', password_confirmation: 'password' )
