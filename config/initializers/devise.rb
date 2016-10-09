fb66a6bbe9cc2e6ba43e5e2300384069bdd26590
Toby Privett <toby.privett@oceanshq.com>
2016-10-09 10:46:39 +0100

    Update mail_sender

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -6,7 +6,7 @@ Devise.setup do |config|
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class with default "from" parameter.
-  config.mailer_sender = "Tools for Teachers <toolsforteachers.org@gmail.com>"
+  config.mailer_sender = "Tools for Teachers <toolsforteachers@snaplab.co.uk>"
 
   # Configure the class responsible to send e-mails.
   # config.mailer = "Devise::Mailer"
